5e5bc2d7f932ff0c44e54d3d96082f87342e5f95
Leticia-proenca <proenca.leticia@pucpr.edu.br>
2025-06-12 14:25:24 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+const divMeuPortfolio = document.getElementById("divMeuPortfolio");
+const divExpCria_jogo = document.getElementById("divExpCria_jogo");
+const divRA = document.getElementById("divRA")
+const divLogica = document.getElementById("divLogica"); 
+const divCiberfisico = document.getElementById("divCiberfisico");
